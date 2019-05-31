fa2782ef415c887d053b60e077a58d271062cfcd
RahulRj09 <rahul18@navgurukul.org>
2019-05-31 11:28:22 +0530

    add. tic tac toe board

--- src/main/java/gamelogic/TicTacToe.java
+++ src/main/java/gamelogic/TicTacToe.java
@@ -1,4 +1,34 @@
 package gamelogic;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[][] board = new String[][]{
+                {"00", "01", "02"},
+                {"10", "11", "12"},
+                {"20", "21", "22"}
+        };
+        while (true) {
+            for (String[] data : board) {
+                System.out.print("\n");
+                for (String data1 : data) {
+                    System.out.print(data1+"|");
+                }
+                System.out.print("\n");
+                System.out.print("__|__|__|");
+            }
+            System.out.println("\n");
+            System.out.println("First Player X");
+            int firstPlayerRow = input.nextInt();
+            int firstPlayerColumn = input.nextInt();
+            board[firstPlayerRow][firstPlayerColumn] = "x";
+            System.out.println("Second Player O");
+            int secondPlayerRow = input.nextInt();
+            int secondPlayerColumn = input.nextInt();
+            board[secondPlayerRow][secondPlayerColumn] = "o";
+        }
+    }
 }
--- src/test/java/gametest/TicTacToeTest.java
+++ src/test/java/gametest/TicTacToeTest.java
@@ -1,4 +1,5 @@
 package gametest;
 
 public class TicTacToeTest {
+
 }
